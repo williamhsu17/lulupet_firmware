a5f0ef8fb1d4b5ce3bf44835ac274d5db566e025
evan <tungchangw@gmail.com>
2021-04-17 16:55:52 +0800

    feat: add app_cmd for command line interface through serial port

---
+++ main/include/app_cmd.h
@@ -0,0 +1,18 @@
+#ifndef _APP_CMD_H_
+#define _APP_CMD_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
+
+void app_cmd_main(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // end of _APP_CMD_H_
