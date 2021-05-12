f5bad9fae6cd493d2319db2e230c7caf3a2330c2
evan <tungchangw@gmail.com>
2021-05-13 00:00:17 +0800

    feat(app_led): add led command queue

--- main/app_led.c
+++ main/app_led.c
@@ -19,8 +19,23 @@
 #define TAG "led_task"
 
 #define LED_TASK_PERIOD_MS 100
+#define LED_CMD_QUEUE_CREATE_RETRY 3
+
+// Message Queue for LED task
+QueueHandle_t led_cmd_que = NULL;
 
 static void led_task(void *pvParameter) {
+    int queue_retry = 0;
+
+    led_cmd_que = xQueueCreate(20, sizeof(unsigned int));
+    while ((led_cmd_que = xQueueCreate(20, sizeof(unsigned int))) == NULL) {
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // wait for a second
+        ESP_LOGW(TAG, "led_cmd_que re-create");
+        if (queue_retry++ > LED_CMD_QUEUE_CREATE_RETRY) {
+            ESP_LOGE(TAG, "led task creation failed");
+            return;
+        }
+    }
 
     for (;;) {
         vTaskDelay(LED_TASK_PERIOD_MS / portTICK_PERIOD_MS);
