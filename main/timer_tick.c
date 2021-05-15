5722d6c22c77d9dd5eecb49071b5c6199d724ea1
evan <tungchangw@gmail.com>
2021-05-15 18:22:27 +0800

    feat: add timer_tick task

---
+++ main/timer_tick.c
@@ -0,0 +1,34 @@
+#include "esp_log.h"
+#include "esp_timer.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TICK_PERIOD_MS 1
+static uint32_t systick = 0;
+
+static void tick_inc(void *arg) {
+    (void)arg;
+    systick++;
+}
+
+uint32_t timer_tick_diff(uint32_t start_tick, uint32_t end_tick) {
+    return (end_tick > start_tick) ? (end_tick - start_tick)
+                                   : (UINT32_MAX - start_tick) + end_tick;
+}
+
+uint32_t timer_tick_ms(void) { return systick; }
+
+void timer_tick_init(void) {
+    /* Create and start a periodic timer interrupt to call tick_inc */
+    const esp_timer_create_args_t periodic_timer_args = {
+        .callback = &tick_inc, .name = "periodic_tick"};
+    esp_timer_handle_t periodic_timer;
+    ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
+    ESP_ERROR_CHECK(
+        esp_timer_start_periodic(periodic_timer, TICK_PERIOD_MS * 1000));
+}
