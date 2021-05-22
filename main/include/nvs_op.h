adddbb85228fd97e7598bd93d2c6b243bf39dd40
evan <tungchangw@gmail.com>
2021-05-23 00:15:08 +0800

    feat(nvs_op): add r/w functions for weight calibration nvs feature

--- main/include/nvs_op.h
+++ main/include/nvs_op.h
@@ -5,15 +5,21 @@
 extern "C" {
 #endif
 
+#include "app_weight.h"
 #include "esp_err.h"
+
+esp_err_t nvs_cali_init(void);
+esp_err_t nvs_cali_read_weight_clai_cb(weight_cali_cb *cali);
+esp_err_t nvs_cali_write_weight_clai_cb(weight_cali_cb *cali);
+esp_err_t nvs_cali_reset_weight_clai_cb(void);
 
 esp_err_t nvs_init(void);
 int32_t nvs_read_wifichecked(void);
+esp_err_t nvs_read_wifi_config(wifi_config_t *sta_config);
+esp_err_t nvs_write_wifi_val(int32_t set_value, wifi_config_t *wifi_config);
 esp_err_t nvs_reset_wifi_val(void);
 esp_err_t nvs_read_lid_token(char *lid, uint32_t lid_len, char *token,
                              uint32_t token_len);
-esp_err_t nvs_read_wifi_config(wifi_config_t *sta_config);
-esp_err_t nvs_write_wifi_val(int32_t set_value, wifi_config_t *wifi_config);
 esp_err_t nvs_write_lid_token(char *lid, uint32_t lid_len, char *token,
                               uint32_t token_len);
 
--- main/nvs_op.c
+++ main/nvs_op.c
@@ -9,71 +9,156 @@
 
 #define TAG "nvs_op"
 
-/* NVS storage define */
+/* nvs storage define */
 #define STORAGE_NAMESPACE "storage"
-#define NVSWIFISETTING "wificonfig"
-#define NVSWIFICHECK "wifichecked"
-#define NVSAPPLID "applid"
-#define NVSAPPTOKEN "apptoken"
+#define NVS_WIFI_SETTING "wificonfig"
+#define NVS_WIFI_CHECK "wifichecked"
+#define NVS_APP_LID "applid"
+#define NVS_APP_TOKEN "apptoken"
 
-static esp_err_t esp_err_print(esp_err_t err, const char *file, uint32_t line);
+#define NVS_CALI_PARTITION "nvs_cali"
+#define NVA_WEIGHT_CALI_NAMESPACE "nvs_cali_weight"
+#define NVS_WEIGHT_CALI_VAL "weight_cali_cb"
 
-static esp_err_t esp_err_print(esp_err_t err, const char *file, uint32_t line) {
-    ESP_LOGE(TAG, "err: %s %s:L%d", esp_err_to_name(err), file, line);
-    return err;
+static esp_err_t esp_err_print(esp_err_t esp_err, const char *file,
+                               uint32_t line);
+
+static esp_err_t esp_err_print(esp_err_t esp_err, const char *file,
+                               uint32_t line) {
+    ESP_LOGE(TAG, "err: %s %s:L%d", esp_err_to_name(esp_err), file, line);
+    return esp_err;
+}
+
+esp_err_t nvs_cali_init(void) {
+    ESP_LOGI(TAG, "%s init", NVS_CALI_PARTITION);
+    esp_err_t esp_err = nvs_flash_init_partition(NVS_CALI_PARTITION);
+    if (esp_err != ESP_OK) {
+        ESP_LOGW(TAG, "%s erase", NVS_CALI_PARTITION);
+        nvs_flash_erase_partition(NVS_CALI_PARTITION);
+        if ((esp_err = nvs_flash_init_partition(NVS_CALI_PARTITION)) !=
+            ESP_OK) {
+            return esp_err_print(esp_err, __func__, __LINE__);
+        }
+    }
+    ESP_LOGW(TAG, "%s init ok", NVS_CALI_PARTITION);
+    return esp_err;
+}
+
+esp_err_t nvs_cali_read_weight_clai_cb(weight_cali_cb *cali) {
+    if (cali == NULL) {
+        return esp_err_print(ESP_ERR_INVALID_ARG, __func__, __LINE__);
+    }
+
+    esp_err_t esp_err;
+    nvs_handle_t h;
+    esp_err = nvs_open_from_partition(
+        NVS_CALI_PARTITION, NVA_WEIGHT_CALI_NAMESPACE, NVS_READONLY, &h);
+    if (esp_err != ESP_OK) {
+        return esp_err_print(esp_err, __func__, __LINE__);
+    }
+
+    uint32_t len = sizeof(weight_cali_cb);
+    esp_err = nvs_get_blob(h, NVS_WEIGHT_CALI_VAL, cali, &len);
+    if (esp_err != ESP_OK) {
+        esp_err_print(esp_err, __func__, __LINE__);
+        goto FINAL;
+    }
+
+    weight_list_cali_val(cali);
+
+FINAL:
+    nvs_close(h);
+    return esp_err;
+}
+
+esp_err_t nvs_cali_write_weight_clai_cb(weight_cali_cb *cali) {
+    if (cali == NULL) {
+        return esp_err_print(ESP_ERR_INVALID_ARG, __func__, __LINE__);
+    }
+
+    esp_err_t esp_err;
+    nvs_handle_t h;
+    esp_err = nvs_open_from_partition(
+        NVS_CALI_PARTITION, NVA_WEIGHT_CALI_NAMESPACE, NVS_READWRITE, &h);
+    if (esp_err != ESP_OK) {
+        return esp_err_print(esp_err, __func__, __LINE__);
+    }
+
+    esp_err =
+        nvs_set_blob(h, NVS_WEIGHT_CALI_VAL, cali, sizeof(weight_cali_cb));
+    if (esp_err != ESP_OK) {
+        esp_err_print(esp_err, __func__, __LINE__);
+        goto FINAL;
+    }
+
+    esp_err = nvs_commit(h);
+    if (esp_err != ESP_OK) {
+        esp_err_print(esp_err, __func__, __LINE__);
+        goto FINAL;
+    }
+
+FINAL:
+    nvs_close(h);
+    return esp_err;
+}
+
+esp_err_t nvs_cali_reset_weight_clai_cb(void) {
+    weight_cali_cb cali;
+
+    memset(&cali, 0x00, sizeof(weight_cali_cb));
+
+    return nvs_cali_write_weight_clai_cb(&cali);
 }
 
 esp_err_t nvs_init(void) {
-    esp_err_t err;
+    esp_err_t esp_err;
+
+    ESP_LOGE(TAG, "sizeof(weight_cali_cb): %d\n\n\n", sizeof(weight_cali_cb));
 
 #if (FUNC_ERASE_NVS_BOOTUP)
     ESP_ERROR_CHECK(nvs_flash_erase());
 #endif
-    err = nvs_flash_init();
+    esp_err = nvs_flash_init();
     ESP_LOGI(TAG, "nvs init");
-    if (err == ESP_ERR_NVS_NO_FREE_PAGES ||
-        err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_LOGI(TAG, "nvs error, erase");
+    if (esp_err == ESP_ERR_NVS_NO_FREE_PAGES ||
+        esp_err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_LOGW(TAG, "nvs erase");
         ESP_ERROR_CHECK(nvs_flash_erase());
-        err = nvs_flash_init();
-        if (err != ESP_OK)
-            esp_err_print(err, __func__, __LINE__);
+        esp_err = nvs_flash_init();
+        if (esp_err != ESP_OK)
+            esp_err_print(esp_err, __func__, __LINE__);
     }
-    ESP_LOGI(TAG, "nvs ready");
-    return err;
+    ESP_LOGW(TAG, "nvs init ok");
+    return esp_err;
 }
 
 int32_t nvs_read_wifichecked(void) {
     nvs_handle_t handle;
-    esp_err_t err;
+    esp_err_t esp_err;
     int32_t wifichecked_value = 0;
     int32_t set_value = 0;
     wifi_config_t wifi_config = {};
 
-    // Open
-    err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &handle);
-    if (err != ESP_OK) {
-        esp_err_print(err, __FUNCTION__, __LINE__);
+    esp_err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &handle);
+    if (esp_err != ESP_OK) {
+        esp_err_print(esp_err, __FUNCTION__, __LINE__);
         nvs_init();
         nvs_reset_wifi_val();
         return wifichecked_value;
     }
 
-    // read nvs NVSWIFICHECK
-    err = nvs_get_i32(handle, NVSWIFICHECK, &wifichecked_value);
-    if (err != ESP_OK) {
-        // if can not read nvs NVSWIFICHECK, write the default value into nvs
+    esp_err = nvs_get_i32(handle, NVS_WIFI_CHECK, &wifichecked_value);
+    if (esp_err != ESP_OK) {
+        // if can not read nvs NVS_WIFI_CHECK, write the default value into nvs
         ESP_LOGW(TAG, "nvs wifichecked not found");
         ESP_LOGW(TAG, "nvs write wifichecked/wificonfig");
-        // Write
-        ESP_ERROR_CHECK(nvs_set_i32(handle, NVSWIFICHECK, set_value));
-        ESP_ERROR_CHECK(nvs_set_blob(handle, NVSWIFISETTING, &wifi_config,
+        ESP_ERROR_CHECK(nvs_set_i32(handle, NVS_WIFI_CHECK, set_value));
+        ESP_ERROR_CHECK(nvs_set_blob(handle, NVS_WIFI_SETTING, &wifi_config,
                                      sizeof(wifi_config)));
-        nvs_get_i32(handle, NVSWIFICHECK, &wifichecked_value);
+        nvs_get_i32(handle, NVS_WIFI_CHECK, &wifichecked_value);
     }
     ESP_LOGI(TAG, "nvs read wifichecked : %d", wifichecked_value);
 
-    // Close
     nvs_close(handle);
     return wifichecked_value;
 }
@@ -87,90 +172,85 @@ esp_err_t nvs_reset_wifi_val(void) {
 esp_err_t nvs_read_lid_token(char *lid, uint32_t lid_len, char *token,
                              uint32_t token_len) {
     nvs_handle_t handle;
-    esp_err_t err;
+    esp_err_t esp_err;
 
     if (lid == NULL || token == NULL || lid_len == 0 || token_len == 0) {
         return esp_err_print(ESP_ERR_INVALID_ARG, __func__, __LINE__);
     }
 
-    // Open
-    err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &handle);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
+    esp_err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &handle);
+    if (esp_err != ESP_OK) {
+        return esp_err_print(esp_err, __func__, __LINE__);
     }
 
-    // Read
-    err = nvs_get_blob(handle, NVSAPPLID, lid, &lid_len);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
+    esp_err = nvs_get_blob(handle, NVS_APP_LID, lid, &lid_len);
+    if (esp_err != ESP_OK) {
+        return esp_err_print(esp_err, __func__, __LINE__);
     }
-    err = nvs_get_blob(handle, NVSAPPTOKEN, token, &token_len);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
+    esp_err = nvs_get_blob(handle, NVS_APP_TOKEN, token, &token_len);
+    if (esp_err != ESP_OK) {
+        return esp_err_print(esp_err, __func__, __LINE__);
     }
     ESP_LOGI(TAG, "nvs lid   : %s", lid);
     ESP_LOGI(TAG, "nvs token : %s", token);
 
-    // Close
     nvs_close(handle);
     return ESP_OK;
 }
 
 esp_err_t nvs_read_wifi_config(wifi_config_t *sta_config) {
     nvs_handle_t handle;
-    esp_err_t err;
+    esp_err_t esp_err;
     memset(sta_config, 0x0, sizeof(wifi_config_t));
-    uint32_t len = sizeof(wifi_config_t);
 
-    // Open
-    err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &handle);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
+    esp_err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &handle);
+    if (esp_err != ESP_OK) {
+        return esp_err_print(esp_err, __func__, __LINE__);
     }
 
-    // Read
-    err = nvs_get_blob(handle, NVSWIFISETTING, sta_config, &len);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
+    uint32_t len = sizeof(wifi_config_t);
+    esp_err = nvs_get_blob(handle, NVS_WIFI_SETTING, sta_config, &len);
+    if (esp_err != ESP_OK) {
+        return esp_err_print(esp_err, __func__, __LINE__);
     }
 
     ESP_LOGI(TAG, "nvs read WiFi configure ssid:%s passwd:%s",
              sta_config->sta.ssid, sta_config->sta.password);
-    // Close
+
     nvs_close(handle);
     return ESP_OK;
 }
 
 esp_err_t nvs_write_wifi_val(int32_t set_value, wifi_config_t *wifi_config) {
     nvs_handle_t handle;
-    esp_err_t err;
+    esp_err_t esp_err;
 
-    // Open
-    err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &handle);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
+    esp_err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &handle);
+    if (esp_err != ESP_OK) {
+        return esp_err_print(esp_err, __func__, __LINE__);
     }
 
-    // Write
     ESP_LOGI(TAG, "save set_value: %d into nvs", set_value);
-    err = nvs_set_i32(handle, NVSWIFICHECK, set_value);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
+    esp_err = nvs_set_i32(handle, NVS_WIFI_CHECK, set_value);
+    if (esp_err != ESP_OK) {
+        esp_err_print(esp_err, __func__, __LINE__);
+        goto FINAL;
     }
     ESP_LOGI(TAG, "save wifi_config into nvs");
-    err = nvs_set_blob(handle, NVSWIFISETTING, wifi_config,
-                       sizeof(wifi_config_t));
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
+    esp_err = nvs_set_blob(handle, NVS_WIFI_SETTING, wifi_config,
+                           sizeof(wifi_config_t));
+    if (esp_err != ESP_OK) {
+        esp_err_print(esp_err, __func__, __LINE__);
+        goto FINAL;
     }
 
-    // Commit
-    err = nvs_commit(handle);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
+    esp_err = nvs_commit(handle);
+    if (esp_err != ESP_OK) {
+        esp_err_print(esp_err, __func__, __LINE__);
+        goto FINAL;
     }
 
-    // Close
+FINAL:
     nvs_close(handle);
     return ESP_OK;
 }
@@ -178,37 +258,38 @@ esp_err_t nvs_write_wifi_val(int32_t set_value, wifi_config_t *wifi_config) {
 esp_err_t nvs_write_lid_token(char *lid, uint32_t lid_len, char *token,
                               uint32_t token_len) {
     nvs_handle_t handle;
-    esp_err_t err;
+    esp_err_t esp_err;
 
     if (lid == NULL || token == NULL) {
         esp_err_print(ESP_ERR_INVALID_ARG, __func__, __LINE__);
         return ESP_ERR_INVALID_ARG;
     }
 
-    // Open
-    err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &handle);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
+    esp_err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &handle);
+    if (esp_err != ESP_OK) {
+        return esp_err_print(esp_err, __func__, __LINE__);
     }
 
-    // Write
     ESP_LOGI(TAG, "save lid: %s into nvs", lid);
-    err = nvs_set_blob(handle, NVSAPPLID, lid, lid_len);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
+    esp_err = nvs_set_blob(handle, NVS_APP_LID, lid, lid_len);
+    if (esp_err != ESP_OK) {
+        esp_err_print(esp_err, __func__, __LINE__);
+        goto FINAL;
     }
     ESP_LOGI(TAG, "save token: %s into nvs", token);
-    err = nvs_set_blob(handle, NVSAPPTOKEN, token, token_len);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
-    }
-    // Commit
-    err = nvs_commit(handle);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
+    esp_err = nvs_set_blob(handle, NVS_APP_TOKEN, token, token_len);
+    if (esp_err != ESP_OK) {
+        esp_err_print(esp_err, __func__, __LINE__);
+        goto FINAL;
     }
 
-    // Close
+    esp_err = nvs_commit(handle);
+    if (esp_err != ESP_OK) {
+        esp_err_print(esp_err, __func__, __LINE__);
+        goto FINAL;
+    }
+
+FINAL:
     nvs_close(handle);
     return ESP_OK;
 }
