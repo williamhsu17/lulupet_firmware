4a7277c70f035d1e33ef22c785040e56f603b8bf
evan <tungchangw@gmail.com>
2021-05-22 18:50:17 +0800

    feat: move nvs functions into nvs_op module

--- main/app_wifi.c
+++ main/app_wifi.c
@@ -1,17 +1,14 @@
 #include "esp_err.h"
 #include "esp_log.h"
 #include "esp_wifi.h"
-#include "lwip/apps/sntp.h"
-#include "nvs.h"
-#include "nvs_flash.h"
-#include "sdkconfig.h"
-#include <string.h>
-#include <time.h>
-
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
 #include "freertos/queue.h"
 #include "freertos/task.h"
+#include "lwip/apps/sntp.h"
+
+#include <string.h>
+#include <time.h>
 
 #include "include/app_key.h"
 #include "include/app_led.h"
@@ -19,20 +16,15 @@
 #include "include/blufi.h"
 #include "include/board_driver.h"
 #include "include/event.h"
+#include "include/nvs_op.h"
 #include "include/task_httpc.h"
 #include "include/util.h"
+#include "sdkconfig.h"
 
 #define TAG "app_wifi"
 
 /* Constants that aren't configurable in menuconfig */
 #define DUMMY_SENSOR 1
-
-/* NVS storage define */
-#define STORAGE_NAMESPACE "storage"
-#define NVSWIFISETTING "wificonfig"
-#define NVSWIFICHECK "wifichecked"
-#define NVSAPPLID "applid"
-#define NVSAPPTOKEN "apptoken"
 
 /* Test WiFi STA configuration */
 #define WIFI_CHECK_WAIT_TIME_MS 10000
@@ -58,7 +50,9 @@ static const int WIFI_CONNECTED_BIT = BIT0;
 static char lulupet_lid_get[NVS_LULUPET_LID_LEN];
 static char lulupet_token_get[NVS_LULUPET_TOKEN_LEN];
 
+#if 0 // unused
 static esp_err_t esp_err_print(esp_err_t err, const char *func, uint32_t line);
+#endif
 static void service_connect_event_handler(void *arg, esp_event_base_t base,
                                           int32_t event_id, void *event_data);
 static void wifi_event_init(void);
@@ -70,135 +64,15 @@ static void sntp_obtain_time(void);
 static void sntp_check(void);
 static void snpt_time_check(void);
 
-static esp_err_t nvs_init(void);
-static int32_t nvs_read_wifichecked(void);
-static esp_err_t nvs_reset_wifi_val(void);
-static esp_err_t nvs_read_lid_token(void);
-static esp_err_t nvs_read_wifi_config(void);
-
+#if 0 // unused
 static esp_err_t esp_err_print(esp_err_t err, const char *func, uint32_t line) {
     ESP_LOGE(TAG, "err:%s %s:L%d", esp_err_to_name(err), func, line);
     return err;
 }
+#endif
 
 static void set_led_cmd(unsigned int led_cmd_load) {
     xQueueSend(led_cmd_que, (void *)&led_cmd_load, (TickType_t)0);
-}
-
-static esp_err_t nvs_init(void) {
-    esp_err_t err;
-
-#if (FUNC_ERASE_NVS_BOOTUP)
-    ESP_ERROR_CHECK(nvs_flash_erase());
-#endif
-    err = nvs_flash_init();
-    ESP_LOGI(TAG, "nvs init");
-    if (err == ESP_ERR_NVS_NO_FREE_PAGES ||
-        err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_LOGI(TAG, "nvs error, erase");
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        err = nvs_flash_init();
-        if (err != ESP_OK)
-            esp_err_print(err, __func__, __LINE__);
-    }
-    ESP_LOGI(TAG, "nvs ready");
-    return err;
-}
-
-static esp_err_t nvs_reset_wifi_val(void) {
-    ESP_LOGI(TAG, "%s:L%d", __func__, __LINE__);
-    wifi_config_t wifi_config = {};
-    return nvs_write_wifi_val(0, &wifi_config);
-}
-
-static esp_err_t nvs_read_lid_token(void) {
-    nvs_handle_t handle;
-    esp_err_t err;
-    uint32_t len;
-
-    // Open
-    err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &handle);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
-    }
-
-    // Read
-    len = sizeof(lulupet_lid_get);
-    err = nvs_get_blob(handle, NVSAPPLID, &lulupet_lid_get[0], &len);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
-    }
-    len = sizeof(lulupet_token_get);
-    err = nvs_get_blob(handle, NVSAPPTOKEN, &lulupet_token_get, &len);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
-    }
-    ESP_LOGI(TAG, "nvs lid   : %s", lulupet_lid_get);
-    ESP_LOGI(TAG, "nvs token : %s", lulupet_token_get);
-
-    // Close
-    nvs_close(handle);
-    return ESP_OK;
-}
-
-static esp_err_t nvs_read_wifi_config(void) {
-    nvs_handle_t handle;
-    esp_err_t err;
-    memset(&sta_config, 0x0, sizeof(sta_config));
-    uint32_t len = sizeof(sta_config);
-
-    // Open
-    err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &handle);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
-    }
-
-    // Read
-    err = nvs_get_blob(handle, NVSWIFISETTING, &sta_config, &len);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
-    }
-
-    ESP_LOGI(TAG, "nvs read WiFi configure ssid:%s passwd:%s",
-             sta_config.sta.ssid, sta_config.sta.password);
-    // Close
-    nvs_close(handle);
-    return ESP_OK;
-}
-
-static int32_t nvs_read_wifichecked(void) {
-    nvs_handle_t handle;
-    esp_err_t err;
-    int32_t wifichecked_value = 0;
-    int32_t set_value = 0;
-    wifi_config_t wifi_config = {};
-
-    // Open
-    err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &handle);
-    if (err != ESP_OK) {
-        esp_err_print(err, __FUNCTION__, __LINE__);
-        nvs_init();
-        nvs_reset_wifi_val();
-        return wifichecked_value;
-    }
-
-    // read nvs NVSWIFICHECK
-    err = nvs_get_i32(handle, NVSWIFICHECK, &wifichecked_value);
-    if (err != ESP_OK) {
-        // if can not read nvs NVSWIFICHECK, write the default value into nvs
-        ESP_LOGW(TAG, "nvs wifichecked not found");
-        ESP_LOGW(TAG, "nvs write wifichecked/wificonfig");
-        // Write
-        ESP_ERROR_CHECK(nvs_set_i32(handle, NVSWIFICHECK, set_value));
-        ESP_ERROR_CHECK(nvs_set_blob(handle, NVSWIFISETTING, &wifi_config,
-                                     sizeof(wifi_config)));
-        nvs_get_i32(handle, NVSWIFICHECK, &wifichecked_value);
-    }
-    ESP_LOGI(TAG, "nvs read wifichecked : %d", wifichecked_value);
-
-    // Close
-    nvs_close(handle);
-    return wifichecked_value;
 }
 
 static void wifi_init_from_nvs(void) {
@@ -374,9 +248,11 @@ static void wifi_start(esp_event_loop_handle_t event_loop) {
 
     wifi_event_init();
     ESP_LOGI(TAG, "load lid token from nvs");
-    nvs_read_lid_token(); // TODO: error handling
+    nvs_read_lid_token(lulupet_lid_get, sizeof(lulupet_lid_get),
+                       lulupet_token_get,
+                       sizeof(lulupet_token_get)); // TODO: error handling
     ESP_LOGI(TAG, "load WiFi Setting from nvs");
-    nvs_read_wifi_config(); // TODO: error handling
+    nvs_read_wifi_config(&sta_config); // TODO: error handling
     wifi_init_from_nvs();
     set_led_cmd(LED_GREEN_1HZ);
     wifi_check_connect(WIFI_CONN_CHK_MS, WIFI_CONN_RETRY);
@@ -384,77 +260,6 @@ static void wifi_start(esp_event_loop_handle_t event_loop) {
     ESP_LOGI(TAG, "connected to AP");
     snpt_time_check();
     ESP_LOGI(TAG, "Update time from SNTP");
-}
-
-esp_err_t nvs_write_wifi_val(int32_t set_value, wifi_config_t *wifi_config) {
-    nvs_handle_t handle;
-    esp_err_t err;
-
-    // Open
-    err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &handle);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
-    }
-
-    // Write
-    ESP_LOGI(TAG, "save set_value: %d into nvs", set_value);
-    err = nvs_set_i32(handle, NVSWIFICHECK, set_value);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
-    }
-    ESP_LOGI(TAG, "save wifi_config into nvs");
-    err = nvs_set_blob(handle, NVSWIFISETTING, wifi_config,
-                       sizeof(wifi_config_t));
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
-    }
-
-    // Commit
-    err = nvs_commit(handle);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
-    }
-
-    // Close
-    nvs_close(handle);
-    return ESP_OK;
-}
-
-esp_err_t nvs_write_lid_token(char *lid, char *token) {
-    nvs_handle_t handle;
-    esp_err_t err;
-
-    if (lid == NULL || token == NULL) {
-        esp_err_print(ESP_ERR_INVALID_ARG, __func__, __LINE__);
-        return ESP_ERR_INVALID_ARG;
-    }
-
-    // Open
-    err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &handle);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
-    }
-
-    // Write
-    ESP_LOGI(TAG, "save lid: %s into nvs", lid);
-    err = nvs_set_blob(handle, NVSAPPLID, lid, sizeof(lulupet_lid_get));
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
-    }
-    ESP_LOGI(TAG, "save token: %s into nvs", token);
-    err = nvs_set_blob(handle, NVSAPPTOKEN, token, sizeof(lulupet_token_get));
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
-    }
-    // Commit
-    err = nvs_commit(handle);
-    if (err != ESP_OK) {
-        return esp_err_print(err, __func__, __LINE__);
-    }
-
-    // Close
-    nvs_close(handle);
-    return ESP_OK;
 }
 
 char *app_wifi_get_lid(void) { return &lulupet_lid_get[0]; }
