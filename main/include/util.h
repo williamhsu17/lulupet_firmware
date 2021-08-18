33d20cfd3ab8c5fd44cacb9aa959d5dbfbd4a73e
evan <tungchangw@gmail.com>
2021-08-18 23:17:18 +0800

    feat: v1.0.0

--- main/include/util.h
+++ main/include/util.h
@@ -8,8 +8,8 @@ extern "C" {
 #include "sdkconfig.h"
 
 // Version
-#define VERSION_MAJOR 0
-#define VERSION_MINOR 2
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
 #define VERSION_PATCH 0
 
 // bitwise
